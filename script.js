6d72ac66d2daf04e6e3b5042d2e56c7c12465c58
V Vikram <vikram710v@gmail.com>
2019-10-24 15:11:12 +0530

    Add edit and delete feature

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var mysql = require('mysql');
 var app = express();
+var bodyParser = require('body-parser');
+var querystring = require('querystring');  
 
 var conn = mysql.createConnection({
     host: "localhost",
@@ -47,6 +49,73 @@ var conn = mysql.createConnection({
       }
     });
   }
+)
+
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+app.post('/add',urlencodedParser,function(req,res){
+
+  var details = [
+    [req.body.name, req.body.type] 
+  ]
+
+  conn.query("INSERT INTO companies(name, type) VALUES (?)",details, function(error, rows, fields){
+    if(!!error){
+      const query = querystring.stringify({
+        "response":"error"
+    });
+      return res.redirect('http://localhost:3000/home/admin'+query);
+    }
+    else{
+      const query = querystring.stringify({
+        "response":"success"
+    });
+      return res.redirect('http://localhost:3000/home/admin?'+query);
+    }
+  });
+}
+)
+
+app.get('/delete/',function(req,res){
+  id = req.query.id
+  conn.query("DELETE FROM companies WHERE id="+id, function(error, rows, fields){
+    if(!!error){
+      const query = querystring.stringify({
+        "response":"error"
+    });
+      return res.redirect('http://localhost:3000/home/admin?'+query);
+    }
+    else{
+      const query = querystring.stringify({
+        "response":"success"
+    });
+      return res.redirect('http://localhost:3000/home/admin?'+query);
+    }
+  });
+}
+)
+
+app.post('/edit',urlencodedParser,function(req,res){
+  var id = req.body.id
+  var details = [
+    req.body.name, req.body.type,id
+  ]
+  conn.query('UPDATE companies SET name=?, type=? WHERE id =?',details, function(error, rows, fields){
+    if(!!error){
+      const query = querystring.stringify({
+        "response":"error"
+    });
+    console.log(error)
+      return res.redirect('http://localhost:3000/home/admin?'+query);
+    }
+    else{
+      const query = querystring.stringify({
+        "response":"success"
+    });
+      return res.redirect('http://localhost:3000/home/admin?'+query);
+    }
+  });
+}
 )
 
   app.listen(5000);
